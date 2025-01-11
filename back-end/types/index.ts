f3e1fa43c8dd2945af9395712160c92f3f31f6d2
IkkiVd <ikki.vdm05@gmail.com>
2025-01-11 20:10:07 +0100

    Added books and profiles to the db

--- back-end/types/index.ts
+++ back-end/types/index.ts
@@ -7,6 +7,20 @@ export type UserInput = {
     role?: Role;
 };
 
+export type BookInput = {
+    id?: number;
+    title: string;
+    author: ProfileInput;
+    description: string;
+    isbn: string;
+};
+
+export type ProfileInput = {
+    id?: number;
+    firstName: string;
+    lastName: string;
+};
+
 export type AuthenticationResponse = {
     token: string;
     username: string;
